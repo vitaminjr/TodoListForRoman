5510361c191e684705063b3bf4ba4d1ed1883272
vitaminjr <vitaminjr4@gmail.com>
2016-06-29 18:07:37 +0300

    homework_2_commit

--- app/src/main/java/com/roman/mobidev/todolist/adapters/TasksCursorAdapter.java
+++ app/src/main/java/com/roman/mobidev/todolist/adapters/TasksCursorAdapter.java
@@ -2,6 +2,8 @@ package com.roman.mobidev.todolist.adapters;
 
 import android.content.Context;
 import android.database.Cursor;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -16,6 +18,7 @@ import com.roman.mobidev.todolist.models.Task;
  * Created by roman on 05.04.16.
  */
 public class TasksCursorAdapter extends CursorAdapter {
+    ViewHolder viewHolder;
 
     public TasksCursorAdapter(Context context, Cursor c, boolean autoRequery) {
         super(context, c, autoRequery);
@@ -23,18 +26,32 @@ public class TasksCursorAdapter extends CursorAdapter {
 
     @Override
     public View newView(Context context, Cursor cursor, ViewGroup parent) {
-        return LayoutInflater.from(context).inflate(R.layout.item_tasks_list, parent, false);
+        View view =  LayoutInflater.from(context).inflate(R.layout.item_tasks_list, parent, false);
+        return view;
     }
 
     @Override
     public void bindView(View view, Context context, Cursor cursor) {
-        TextView taskName = (TextView) view.findViewById(R.id.i_task_name_textView);
-        // Extract properties from cursor
-        String name = cursor.getString(TaskFields.NAME.ordinal());
-//        int priority = cursor.getInt(cursor.getColumnIndexOrThrow("priority"));
-        // Populate fields with extracted properties
-        taskName.setText(name);
 
+        viewHolder = (ViewHolder) view.getTag();
+
+        if (viewHolder == null){
+      //      LayoutInflater inflater = (LayoutInflater) context
+      //              .getSystemService(Context.LAYOUT_INFLATER_SERVICE);
+    //        view = inflater.inflate(R.layout.item_tasks_list, null, true);
+            viewHolder = new ViewHolder();
+            Log.d("Log","use_method_new_view");
+            viewHolder.textView = (TextView) view.findViewById(R.id.i_task_name_textView);
+            view.setTag(viewHolder);
+        }
+        else
+        {
+            viewHolder = (ViewHolder) view.getTag();
+            Log.d("Log","get");
+        }
+        Log.d("Log","iterator");
+        String name = cursor.getString(TaskFields.NAME.ordinal());
+        viewHolder.textView.setText(name);
     }
 
     public Task getTaskFromPosition(int position){
@@ -44,5 +61,9 @@ public class TasksCursorAdapter extends CursorAdapter {
             task = Task.createFromCursor(cursor);
         }
         return task;
+    }
+
+    static class ViewHolder {
+        TextView textView;
     }
 }
